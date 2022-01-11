02edcea52bca71978a4046d8e4f321bba31ce8b0
jennharw <73508138+jennharw@users.noreply.github.com>
2022-01-11 16:35:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
+#client = MongoClient('localhost', 27017)
 #client = MongoClient('mongodb://test:test@localhost', 27017)
 
 db = client.soccer
@@ -121,5 +121,5 @@ def hello_world():
 #     return jsonify({'result': 'success', 'msg':  f'{name_receive} 저장완료 !'})
 
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run('0.0.0.0', port=5000, debug=True)
